5c837387cfd1b0daf591ff20453c4a43fcde7154
AKABigDinner <109917770+AKABigDinner@users.noreply.github.com>
2024-07-13 01:39:45 -0400

    Update and rename PalWorld-Install_v2.sh to install2.sh

--- install2.sh
+++ install2.sh
@@ -1,46 +1,14 @@
 #!/bin/bash
 set -x
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl
-sudo apt update && sudo apt upgrade -y
-sudo apt install git build-essential cmake -y
-git clone https://github.com/ptitSeb/box86
-sudo dpkg --add-architecture armhf
-sudo apt update
-sudo apt install gcc-arm-linux-gnueabihf libc6:armhf -y
-cd ~/box86
-mkdir build && cd build
-cmake .. -DRPI4ARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
-make -j$(nproc)
-sudo make install
-sudo systemctl restart systemd-binfmt
-sudo apt update && sudo apt upgrade -y
-sudo apt install git build-essential cmake -y
-sudo apt install git build-essential cmake
-git clone https://github.com/ptitSeb/box64.git
-cd ~/box64
-mkdir build && cd build
-cmake .. -DRPI4ARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
-make -j$(nproc)
-sudo make install
-sudo systemctl restart systemd-binfmt
-sudo useradd palworld -m
-cd ~
-
-sudo -u palworld bash << EOF
 mkdir ~/steamcmd && cd ~/steamcmd
 sleep 5
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
-
 ./steamcmd.sh & pid=$!
 sleep 5m
 kill -TSTP $pid
-
 ./steamcmd.sh +force_install_dir ~/steamworkssdk +@sSteamCmdForcePlatformType linux +login anonymous +app_update 1007 validate +quit
 mkdir -p ~/.steam/sdk64 && cp ~/steamworkssdk/linux64/steamclient.so ~/.steam/sdk64/
-
 ./steamcmd.sh +force_install_dir ~/palworldserver +@sSteamCmdForcePlatformType linux +login anonymous +app_update 2394010 validate +quit
-
 cd ~/palworldserver/
 ./PalServer.sh & pid=$!
 sleep 5m
@@ -48,7 +16,6 @@ kill -TSTP $pid
 sleep 5
 cp ~/palworldserver/DefaultPalWorldSettings.ini ~/palworldserver/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
 sleep 5
-EOF
 
 echo "[Unit]
 Description=Palworld Server
@@ -65,8 +32,4 @@ Restart=always
 
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/palworld.service
-sleep 10
-sudo systemctl enable palworld
-sleep 10
-sudo systemctl start palworld
-sleep 30
+
